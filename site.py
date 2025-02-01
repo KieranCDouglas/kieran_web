15380827962ff0817972d068fb7f921e7c94818a
KieranCDouglas <kieran@arizona.edu>
2025-02-01 11:42:30 -0700

    website outline setup
    - cramming a bunch of stuff into the site to provide a framework

--- site.py
+++ site.py
@@ -1,1 +1,24 @@
-### website code
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/academic')
+def academic():
+    return render_template('academic.html')
+
+@app.route('/art')
+def art():
+    return render_template('art.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    app.run(debug=True)
